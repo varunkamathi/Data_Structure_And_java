c79c7b67d1196d4f055998d260d9218848ff84fd
varunkamathi <varunkamathi522004@gmail.com>
2024-03-09 22:12:13 +0530

    binary

---
+++ Binary_Tree.java
@@ -0,0 +1,34 @@
+public class Binary_Tree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+            
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildtree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildtree(nodes);
+            newNode.right = buildtree(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildtree(nodes);
+        System.out.println(root.data);
+    }
+}
