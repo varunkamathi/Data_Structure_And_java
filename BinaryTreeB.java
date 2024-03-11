894b3d18bb322b8b203710f078ed770b5f09c29b
varunkamathi <varunkamathi522004@gmail.com>
2024-03-11 15:56:32 +0530

    update

--- BinaryTreeB.java
+++ BinaryTreeB.java
@@ -88,7 +88,7 @@ public class BinaryTreeB {
                 }
             }
         }
-        
+
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
@@ -99,6 +99,7 @@ public class BinaryTreeB {
         //tree.inOrder(root);
         //tree.postOrder(root);
         tree.levelOrder(root);
+        
 
     }
 }
