9b170d454b451950b828c8320eb4d92162670e6b
varunkamathi <varunkamathi522004@gmail.com>
2024-03-31 12:22:26 +0530

    priority queue

---
+++ Heaps/PQ.java
@@ -0,0 +1,21 @@
+package Heaps;
+import java.util.PriorityQueue;
+import java.util.*;
+
+public class PQ {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(1);
+        pq.add(4);
+        pq.add(2);
+        pq.add(5);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());//O(1)
+            pq.remove();//O(logn)
+        }
+
+    }
+    
+}
