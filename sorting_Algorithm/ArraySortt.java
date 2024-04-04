7b30367846b143faf72a5562c6c56dedbf985050
varunkamathi <varunkamathi522004@gmail.com>
2024-04-04 21:29:46 +0530

    Array sort

---
+++ sorting_Algorithm/ArraySortt.java
@@ -0,0 +1,15 @@
+package sorting_Algorithm;
+
+import java.util.Arrays;
+
+public class ArraySortt {
+    int [] arr = {8, 5, 2, 6, 1};
+public static void main(String[] args) {
+    int a[] = {8,5,2,6,1};
+
+    System.out.println(a);
+    Arrays.sort(a);
+    System.out.println(a);
+
+}
+}
