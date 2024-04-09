e12c4710473feb59fc2b0298c5f8331ca7720519
varunkamathi <varunkamathi522004@gmail.com>
2024-04-09 19:53:44 +0530

    using Exception handling

---
+++ Exception_Handling/Grace_marks_system.java
@@ -0,0 +1,29 @@
+package Exception_Handling;
+
+import java.util.Scanner;
+
+public class Grace_marks_system {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the marks: ");
+            int marks = sc.nextInt();
+
+            if(marks >= 20){
+                System.out.println("Pass exam");
+            }
+
+        try {
+          if(marks >= 15 && marks <= 20){
+            int Gmarks = 20 - marks ;
+            marks = marks + Gmarks;
+            System.out.println("pass exam with grace makrs :- " + marks);
+          }else {
+            System.out.println("fail exam");
+          }
+
+        } catch (Exception e) {
+            System.out.println("Invalid number");
+            // TODO: handle exception
+        }
+    }
+}
