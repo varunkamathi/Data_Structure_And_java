df83c1a55ea9e5448dd8029547217e2f29f0d239
varunkamathi <varunkamathi522004@gmail.com>
2024-04-09 17:54:08 +0530

    selectionsort

--- java_collection_framework/map.java
+++ java_collection_framework/map.java
@@ -5,12 +5,22 @@ import java.util.TreeMap;
 
 public class map {
   public static void main(String[] args) {
-   // HashMap<String, Integer> obj = new HashMap<>();
-   TreeMap<String,Integer> obj = new TreeMap<>();
-    obj.put("varun", 1);
-    obj.put("Aniket", 2);
-    System.out.println(obj);
-    obj.putAll(obj);
+   HashMap<String, Integer> obj = new HashMap<>();
+   obj.put("Varun", 1);
+   obj.put("yash", 20);
+   System.out.println(obj);
+   obj.putAll(obj);
+
+   System.out.println(obj.get("Varun"));
+System.out.println(obj.containsKey("yash"));
+System.out.println(obj.remove("yash"));
+System.out.println(obj);
+   
+   TreeMap<String,Integer> obj1 = new TreeMap<>();
+    obj1.put("varun", 8);
+    obj1.put("Aniket", 4);
+    System.out.println(obj1);
+    obj1.putAll(obj1);
 
 
 }
