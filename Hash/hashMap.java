10ad6cea078ec7b882bdc2f9712704b9ddbef4a9
varunkamathi <varunkamathi522004@gmail.com>
2024-04-11 12:37:35 +0530

    hash map And tree map

---
+++ Hash/hashMap.java
@@ -0,0 +1,28 @@
+package Hash;
+
+/**
+ * hashMap
+ */
+import java.util.HashMap;
+import java.util.TreeMap;
+public class hashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> obj = new HashMap<>();
+        obj.put("Varun", 1);
+        obj.put("yash", 20);
+        System.out.println(obj);
+        obj.putAll(obj);
+     
+        System.out.println(obj.get("Varun"));
+     System.out.println(obj.containsKey("yash"));
+     System.out.println(obj.remove("yash"));
+     System.out.println(obj);
+        
+        TreeMap<String,Integer> obj1 = new TreeMap<>();
+         obj1.put("varun", 8);
+         obj1.put("Aniket", 4);
+         System.out.println(obj1);
+         obj1.putAll(obj1);
+     
+    
+}}
