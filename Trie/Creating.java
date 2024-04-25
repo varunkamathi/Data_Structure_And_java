409c2967627b70b005266db914ce8b4290453dac
varunkamathi <varunkamathi522004@gmail.com>
2024-04-25 20:29:33 +0530

    insert element in tries

--- Trie/Creating.java
+++ Trie/Creating.java
@@ -15,7 +15,23 @@ public class Creating {
     }
     public static Node root = new Node();
 
+    public static void insert(String word){
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
     }
 }
