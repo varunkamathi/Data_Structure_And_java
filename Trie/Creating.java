5c811b95697338bdbababba97dc9ea81f16e3829
varunkamathi <varunkamathi522004@gmail.com>
2024-04-26 09:36:33 +0530

    searching in trie

--- Trie/Creating.java
+++ Trie/Creating.java
@@ -27,11 +27,25 @@ public class Creating {
         curr.eow = true;
     }
 
+    public static boolean search(String key){
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow = true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
 
         for (int i = 0; i < words.length; i++) {
             insert(words[i]);
         }
+
+        System.out.println(search("a"));
     }
 }
