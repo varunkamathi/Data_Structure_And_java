e8508caa3fbb1af903a57e573ddd38a3d7b8d4ce
varunkamathi <varunkamathi522004@gmail.com>
2024-11-12 11:15:56 +0530

    array

---
+++ Array/Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Array {public static void findMinMax(int[] arr) {
+    int min = arr[0];
+    int max = arr[0];
+    
+    for (int i = 1; i < arr.length; i=i+1) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    System.out.println("Minimum element: " + min);
+    System.out.println("Maximum element: " + max);
+}
+
+public static void main(String[] args) {
+    int[] array = {3, 5, 7, 2, 8, -1, 4, 10, 12};
+    findMinMax(array);
+}
+}
+
---
+++ Array/PrintArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] a = {1,23,3,4,5,6,7};
+        for(int i= 0; i< a.length ; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
